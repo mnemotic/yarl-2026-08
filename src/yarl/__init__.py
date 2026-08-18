d6f929d7e3eb3c1656f1c9d40cb8fb68cffb09ff
Martin Green <mnemotic@pm.me>
2026-08-18 14:24:32 +0300

    Prepare release v0.6.0

--- src/yarl/__init__.py
+++ src/yarl/__init__.py
@@ -6,10 +6,10 @@ import tcod
 YARL_DATA_DIR = sys._MEIPASS if hasattr(sys, "_MEIPASS") else path.dirname(__file__)
 YARL_ASSET_DIR = path.abspath(path.join(YARL_DATA_DIR, "assets"))
 
-__version__ = "0.1.0"
+__version__ = "0.6.0"
 
 tcod.lib.SDL_SetAppMetadata(
     b"YARL",
     bytes(__version__.encode()),
-    b"dev.mnemotic.yarl-202608",
+    b"dev.mnemotic.yarl2026",
 )
